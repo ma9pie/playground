17637fb2ddfdbce9129291b33345aff3b8ca0101
ma9pie <ma9pie.dev@gmail.com>
2026-01-17 20:02:08 +0900

    chore: 데이터 개수 선택 옵션 추가

--- src/pages/TableTest/common/constants/index.ts
+++ src/pages/TableTest/common/constants/index.ts
@@ -1,5 +1,7 @@
 export const DEFAULT_DATA_COUNT = 1000;
 
-export const DATA_COUNTS = [10, 100, 500, 1000, 3000, 5000, 10000];
+export const DATA_COUNTS = [
+  10, 100, 500, 1000, 3000, 5000, 10000, 30000, 50000, 100000,
+];
 
 export const DATA_COUNT_STORAGE_KEY = "table_data_count";

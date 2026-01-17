fbcb48db3587c7084ccf146d2a98bda72d5ecf20
ma9pie <ma9pie.dev@gmail.com>
2026-01-17 20:35:47 +0900

    feat: 업데이트 소요 소긴 측정 기능 추가

--- src/pages/TableTest/Component/index.tsx
+++ src/pages/TableTest/Component/index.tsx
@@ -1,5 +1,12 @@
 import styled from "@emotion/styled";
-import { useCallback, useMemo, useState, useTransition } from "react";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+  useTransition,
+} from "react";
 
 import Sorter from "@/components/Sorter";
 import {
@@ -30,6 +37,9 @@ const Component = ({ data }: IProps) => {
     direction: "NONE",
   });
 
+  const startTimeRef = useRef<number>(0);
+  const [totalRenderTime, setTotalRenderTime] = useState<number>(0);
+
   const sortedData = useMemo(() => {
     const { key, direction } = sortConfig;
     if (direction === "NONE" || !key) return data;
@@ -51,6 +61,9 @@ const Component = ({ data }: IProps) => {
 
   const onSortChange = useCallback(
     (key: string) => (direction: ISortConfig["direction"]) => {
+      setTotalRenderTime(0);
+      startTimeRef.current = performance.now();
+
       startTransition(() => {
         setSortConfig({ key, direction });
       });
@@ -58,134 +71,192 @@ const Component = ({ data }: IProps) => {
     []
   );
 
+  useEffect(() => {
+    if (!isPending && startTimeRef.current !== 0) {
+      const endTime = performance.now();
+      setTotalRenderTime(endTime - startTimeRef.current);
+    }
+  }, [isPending]);
+
   return (
-    <ScrollContainer isPending={isPending}>
-      <Table>
-        <THead>
-          <Tr>
-            <FirstTh rowSpan={2} style={{ width: 150 }}>
-              <Remarks>비고</Remarks>
-            </FirstTh>
-            <Th colSpan={4}>KOSPI</Th>
-            <Th colSpan={3}>NASDAQ</Th>
-            <Th colSpan={2} style={{ borderRight: 0 }}>
-              CRYPTO
-            </Th>
-            <LastTh colSpan={2} style={{ width: 160, right: 0 }}>
-              합계
-            </LastTh>
-          </Tr>
+    <Container>
+      <RenderInfo>
+        최근 업데이트 소요 시간:{" "}
+        {isPending ? (
+          <LoadingText>업데이트 중...</LoadingText>
+        ) : totalRenderTime > 0 ? (
+          `${(totalRenderTime / 1000).toFixed(3)}초`
+        ) : null}
+      </RenderInfo>
+      <ScrollContainer isPending={isPending}>
+        <Table>
+          <THead>
+            <Tr>
+              <FirstTh rowSpan={2} style={{ width: 150 }}>
+                <Remarks>비고</Remarks>
+              </FirstTh>
+              <Th colSpan={4}>KOSPI</Th>
+              <Th colSpan={3}>NASDAQ</Th>
+              <Th colSpan={2} style={{ borderRight: 0 }}>
+                CRYPTO
+              </Th>
+              <LastTh colSpan={2} style={{ width: 160, right: 0 }}>
+                합계
+              </LastTh>
+            </Tr>
 
-          <Tr>
-            {/* KOSPI */}
-            <Th>
-              <Sorter
-                onSort={onSortChange("kospi.total")}
-                direction={getSortDirection("kospi.total")}
-              >
-                전체
-              </Sorter>
-            </Th>
-            <Th>
-              <Sorter
-                onSort={onSortChange("kospi.krw")}
-                direction={getSortDirection("kospi.krw")}
-              >
-                원화
-              </Sorter>
-            </Th>
-            <Th>
-              <Sorter
-                onSort={onSortChange("kospi.usd")}
-                direction={getSortDirection("kospi.usd")}
-              >
-                달러
-              </Sorter>
-            </Th>
-            <Th>
-              <Sorter
-                onSort={onSortChange("kospi.rate")}
-                direction={getSortDirection("kospi.rate")}
-              >
-                변동률
-              </Sorter>
-            </Th>
+            <Tr>
+              {/* KOSPI */}
+              <Th>
+                <Sorter
+                  onSort={onSortChange("kospi.total")}
+                  direction={getSortDirection("kospi.total")}
+                >
+                  전체
+                </Sorter>
+              </Th>
+              <Th>
+                <Sorter
+                  onSort={onSortChange("kospi.krw")}
+                  direction={getSortDirection("kospi.krw")}
+                >
+                  원화
+                </Sorter>
+              </Th>
+              <Th>
+                <Sorter
+                  onSort={onSortChange("kospi.usd")}
+                  direction={getSortDirection("kospi.usd")}
+                >
+                  달러
+                </Sorter>
+              </Th>
+              <Th>
+                <Sorter
+                  onSort={onSortChange("kospi.rate")}
+                  direction={getSortDirection("kospi.rate")}
+                >
+                  변동률
+                </Sorter>
+              </Th>
 
-            {/* NASDAQ */}
-            <Th>
-              <Sorter
-                onSort={onSortChange("nasdaq.total")}
-                direction={getSortDirection("nasdaq.total")}
-              >
-                전체
-              </Sorter>
-            </Th>
-            <Th>
-              <Sorter
-                onSort={onSortChange("nasdaq.usd")}
-                direction={getSortDirection("nasdaq.usd")}
-              >
-                달러
-              </Sorter>
-            </Th>
-            <Th>
-              <Sorter
-                onSort={onSortChange("nasdaq.rate")}
-                direction={getSortDirection("nasdaq.rate")}
-              >
-                변동률
-              </Sorter>
-            </Th>
+              {/* NASDAQ */}
+              <Th>
+                <Sorter
+                  onSort={onSortChange("nasdaq.total")}
+                  direction={getSortDirection("nasdaq.total")}
+                >
+                  전체
+                </Sorter>
+              </Th>
+              <Th>
+                <Sorter
+                  onSort={onSortChange("nasdaq.usd")}
+                  direction={getSortDirection("nasdaq.usd")}
+                >
+                  달러
+                </Sorter>
+              </Th>
+              <Th>
+                <Sorter
+                  onSort={onSortChange("nasdaq.rate")}
+                  direction={getSortDirection("nasdaq.rate")}
+                >
+                  변동률
+                </Sorter>
+              </Th>
 
-            {/* CRYPTO */}
-            <Th>
-              <Sorter
-                onSort={onSortChange("crypto.total")}
-                direction={getSortDirection("crypto.total")}
-              >
-                전체
-              </Sorter>
-            </Th>
-            <Th style={{ borderRight: 0 }}>
-              <Sorter
-                onSort={onSortChange("crypto.rate")}
-                direction={getSortDirection("crypto.rate")}
-              >
-                변동률
-              </Sorter>
-            </Th>
+              {/* CRYPTO */}
+              <Th>
+                <Sorter
+                  onSort={onSortChange("crypto.total")}
+                  direction={getSortDirection("crypto.total")}
+                >
+                  전체
+                </Sorter>
+              </Th>
+              <Th style={{ borderRight: 0 }}>
+                <Sorter
+                  onSort={onSortChange("crypto.rate")}
+                  direction={getSortDirection("crypto.rate")}
+                >
+                  변동률
+                </Sorter>
+              </Th>
 
-            {/* 합계 */}
-            <LastTh style={{ width: 80, right: 80 }}>
-              <Sorter
-                onSort={onSortChange("total.sum")}
-                direction={getSortDirection("total.sum")}
-              >
-                전체
-              </Sorter>
-            </LastTh>
-            <LastTh style={{ width: 80, right: 0 }}>
-              <Sorter
-                onSort={onSortChange("total.rate")}
-                direction={getSortDirection("total.rate")}
-              >
-                변동률
-              </Sorter>
-            </LastTh>
-          </Tr>
-        </THead>
+              {/* 합계 */}
+              <LastTh style={{ width: 80, right: 80 }}>
+                <Sorter
+                  onSort={onSortChange("total.sum")}
+                  direction={getSortDirection("total.sum")}
+                >
+                  전체
+                </Sorter>
+              </LastTh>
+              <LastTh style={{ width: 80, right: 0 }}>
+                <Sorter
+                  onSort={onSortChange("total.rate")}
+                  direction={getSortDirection("total.rate")}
+                >
+                  변동률
+                </Sorter>
+              </LastTh>
+            </Tr>
+          </THead>
 
-        <TBody>
-          {sortedData.map((item: any) => (
-            <Row key={item.id} data={item}></Row>
-          ))}
-        </TBody>
-      </Table>
-    </ScrollContainer>
+          <TBody>
+            {sortedData.map((item: any) => (
+              <Row key={item.id} data={item}></Row>
+            ))}
+          </TBody>
+        </Table>
+      </ScrollContainer>
+    </Container>
   );
 };
 
 export default Component;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const RenderInfo = styled.div`
+  font-size: 13px;
+  color: #4b5563;
+  font-weight: 600;
+  height: 24px;
+  display: flex;
+  align-items: center;
+`;
+
+const LoadingText = styled.span`
+  color: #2563eb;
+  &::after {
+    content: "";
+    display: inline-block;
+    width: 12px;
+    animation: dots 1.5s infinite;
+  }
+  @keyframes dots {
+    0%,
+    20% {
+      content: ".";
+    }
+    40% {
+      content: "..";
+    }
+    60% {
+      content: "...";
+    }
+    80%,
+    100% {
+      content: "";
+    }
+  }
+`;
 
 const ScrollContainer = styled.div<{ isPending: boolean }>`
   width: 100%;
